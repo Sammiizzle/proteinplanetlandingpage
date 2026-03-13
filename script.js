80131df6f436fbd9d64d1744e9b4e5163a76ce21
Samantha Diaz <samanthadiaz2140@gmail.com>
2026-03-13 18:17:46 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const countdownElement = document.getElementById("countdown")
+
+const launchDate = new Date("July 1, 2026 00:00:00").getTime()
+
+const timer = setInterval(function(){
+
+const now = new Date().getTime()
+
+const distance = launchDate - now
+
+const days = Math.floor(distance / (1000*60*60*24))
+const hours = Math.floor((distance%(1000*60*60*24))/(1000*60*60))
+const minutes = Math.floor((distance%(1000*60*60))/(1000*60))
+const seconds = Math.floor((distance%(1000*60))/1000)
+
+countdownElement.innerHTML =
+days+"d "+hours+"h "+minutes+"m "+seconds+"s"
+
+},1000)
